4e80dbcda2b1a0ad4992d3c1fff8f2d45ed2d66a
Rhuan Barros <rhuanbarros@gmail.com>
2022-12-05 14:30:12 -0300

    ajuste menu

--- Supabase/CreateTables.sql
+++
@@ -1,9 +0,0 @@
-create table "public"."UsuarioPerfil" (
-  id uuid references auth.users not null,
-  Email text,
-  NomeCompleto text,
-  primary key (id)
-);
-
-alter table
-  public.profiles enable row level security;
