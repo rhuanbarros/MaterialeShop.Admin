3d0b775b895b39ec6b7bc70f75223e37b3484d8f
Rhuan Barros <rhuanbarros@gmail.com>
2022-12-05 15:18:30 -0300

    mudanca para generico

--- Src/Pages/UsuarioPerfilFolder/UsuarioPerfilPage.razor.cs
+++ Src/Pages/UsuarioPerfilFolder/UsuarioPerfilPage.razor.cs
@@ -10,31 +10,16 @@ public partial class UsuarioPerfilPage
     protected override async Task OnInitializedAsync()
     {
         await GetTable();
-
-        IReadOnlyList<UsuarioPerfil> perfil = await UsuarioPerfilService.GetByUserId("a7251a08-9573-4192-8d14-d4de1ab0ee6e");
-        
-        Console.WriteLine("perfil.Single()");
-        Console.WriteLine(perfil.Single().Email);
-        
-
-        // foreach (var item in perfil)
-        // {
-        //     Console.WriteLine("item");
-        //     Console.WriteLine(item.Email);
-        //     Console.WriteLine(item.NomeCompleto);
-        // }
     }
 
     // ---------------- SELECT TABLE
-    private IReadOnlyList<UsuarioPerfil>? _listaList { get; set; }
-    private IReadOnlyList<UsuarioPerfil>? _listaListFiltered { get; set; }
+    private IReadOnlyList<UsuarioPerfil>? _tableList { get; set; }
+    private IReadOnlyList<UsuarioPerfil>? _tableListFiltered { get; set; }
     private MudTable<UsuarioPerfil>? table;
     protected async Task GetTable()
     {
-        // await Task.Delay(10000);
-        IReadOnlyList<UsuarioPerfil> listas = await UsuarioPerfilService.From();
-        _listaList = listas;
-        _listaListFiltered = listas;
+        _tableList = await UsuarioPerfilService.From();
+        _tableListFiltered = _tableList;
         await InvokeAsync(StateHasChanged);
     }
 
