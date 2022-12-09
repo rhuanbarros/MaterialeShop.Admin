529f017fd34267c6c90cfcb0e819cb09fbe19dd8
Rhuan Barros <rhuanbarros@gmail.com>
2022-12-09 19:46:55 -0300

    limpeza

--- Src/Pages/OrcamentosFolder/AguardandoOrcamentoFolder/AguardandoOrcamentoPage.razor.cs
+++ Src/Pages/OrcamentosFolder/AguardandoOrcamentoFolder/AguardandoOrcamentoPage.razor.cs
@@ -10,12 +10,6 @@ public partial class AguardandoOrcamentoPage
     [Inject] 
     protected ListasViewService ListasViewService {get; set;}
 
-    protected override async Task OnParametersSetAsync()
-    {
-        await GetTable();
-        await GetTableUsuarioPerfil();
-    }
-
     // ---------------- SELECT TABLE
     protected override async Task GetTable()
     {
@@ -42,68 +36,5 @@ public partial class AguardandoOrcamentoPage
         _tableListFiltered = _tableList?.Where(predicate).ToList();
     }
 
-    // ---------------- CLICK NA LINHA DA TABELA
-    private void RowClickEvent(TableRowClickEventArgs<ListasView> e)
-    {
-        NavigationManager.NavigateTo($"/lista/{e.Item.ListaId}");
-    }
-
-    protected override Lista model {get;set;} = new()
-    {
-        Status = "Em criação"
-    };
-
-    // ---------------- CREATE NEW
-    protected override Lista CreateNewModel()
-    {
-        model = new()
-        {
-            Status = "Em criação",
-            CreatedAt = DateTime.Now
-        };
-        return model;
-    }
-
-    
-    protected override async Task OnClickCancel()
-    {
-        //precisei sobrescrever o OnClickCancel pq essa classe nao tava chamando o CreateNewModel dessa classe e sim das classes base
-        form?.Reset();
-        model = CreateNewModel();
-    }
-
-    // ---------------- DELETE
-    protected override Lista SetModelIdToDelete(ListasView item)
-    {
-        return new Lista()
-            {
-                Id = item.ListaId
-            };
-    }
-
-    // ---------------- EDIT MODEL
-    protected override Lista SetModelIdToEdit(ListasView item)
-    {
-        return new Lista()
-        {
-            Id = item.ListaId,
-            PerfilId = item.PerfilId,
-            Endereco = item.Endereco,
-            Status = item.Status
-        };
-    }
-
-    // -------------------START------------------- CAMPO UsuarioPerfil no MODEL  ----------------------------------------
-
-    // ---------------- SELECT TABLE UsuarioPerfil
-    protected IReadOnlyList<Perfil>? _UsuarioPerfilList { get; set; }
-    protected async Task GetTableUsuarioPerfil()
-    {
-        _UsuarioPerfilList = await CrudService.SelectAllFrom<Perfil>();
-        await InvokeAsync(StateHasChanged);
-    }
-
-    private Func<Perfil, string> convertFuncPapel = ci => ci?.NomeCompleto;
-    // -----------------END--------------------- CAMPO UsuarioPerfil no MODEL  ----------------------------------------
 
 }
