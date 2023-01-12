308d56c34aec14e8576b206a7ad4588f3bb06090
Rhuan Barros <rhuanbarros@gmail.com>
2023-01-12 17:10:23 -0300

    comparativo funcionando

--- Src/Pages/OrcamentosFolder/ComparativoFolder/Comparativo.razor.cs
+++ Src/Pages/OrcamentosFolder/ComparativoFolder/Comparativo.razor.cs
@@ -19,12 +19,7 @@ public partial class Comparativo
     [Inject]
     OrcamentoItemService OrcamentoItemService { get; set; }
 
-    private int quantidade_linhas {get; set;}
-    private IEnumerator<ListaItem> _ListaItemListEnumerator;
-    private IEnumerator<OrcamentoView> _OrcamentoViewListEnumerator;
-    private IEnumerator<List<OrcamentoItem>> _OrcamentoItemListEnumerator;
-
-    List<List<string>> linhas = new();
+    public List<List<string>> tabelaComparativa;
 
     protected override async Task OnParametersSetAsync()
     {
@@ -32,91 +27,17 @@ public partial class Comparativo
         await GetOrcamentoView(ListaId);
         await GetOrcamentoItemList(_OrcamentoViewList);
 
-        // retorna o maior valor entre as variaveis. 
-        // no caso, retona a quantidade maior de linhas que serão necessárias inserir na tabela
-        quantidade_linhas = new [] { _ListaItemList.Count, _OrcamentoViewList.Count, _OrcamentoItemList.Count}.Max();
-
-        Console.WriteLine("================================================================");
-        Console.WriteLine("Quantidade de item em cada lista");
-        Console.WriteLine("_ListaItemList.Count");
-        Console.WriteLine(_ListaItemList.Count);
-        Console.WriteLine("_OrcamentoViewList.Count");
-        Console.WriteLine(_OrcamentoViewList.Count);
-        Console.WriteLine("_OrcamentoItemList.Count");
-        Console.WriteLine(_OrcamentoItemList.Count);
-        Console.WriteLine("================================================================");
-
-        _ListaItemListEnumerator = _ListaItemList.GetEnumerator();
-        _OrcamentoViewListEnumerator = _OrcamentoViewList.GetEnumerator();
-        _OrcamentoItemListEnumerator = _OrcamentoItemList.GetEnumerator();
-
-        
-        bool temMais;
-        string conteudo;
-        for (int i = 0; i < quantidade_linhas; i++)
-        {
-            //COLUNA 1 - NUMERO DA LINHA DA TABELA COMPARATIVA DE ORÇAMENTOS
-            List<string> linha = new();
-            linha.Add((i+1).ToString());
-
-            //ITEM SOLICITADO PELO CLIENTE
-
-                //COLUNA 2 - CAMPO QUANTIDADE
-                temMais = _ListaItemListEnumerator.MoveNext();
-                conteudo = temMais ? ( _ListaItemListEnumerator.Current.Quantidade != null ? _ListaItemListEnumerator.Current.Quantidade.ToString() : "" ) : "";
-                linha.Add(conteudo);
-                
-                //COLUNA 3 - CAMPO DESCRICAO
-                conteudo = temMais ? ( _ListaItemListEnumerator.Current.Descricao != null ? _ListaItemListEnumerator.Current.Descricao.ToString() : "" ) : "";
-                linha.Add(conteudo);
-            
-            //COLUNA 4 - COLUNA EM BRANCO SEPARADORA
-            linha.Add("");
-
-            //COLUNAS DE ITENS DO ORÇAMENTOS DAS LOJAS
-                //para cada loja, insere as colunas correspondentes
-            temMais = _OrcamentoViewListEnumerator.MoveNext();
-
-
-            linha.Add("x");
-            linha.Add("x");
-            linha.Add("x");
-            linha.Add("x");
-            linha.Add("x");
-            linha.Add("x");
-            linha.Add("x");
-
-            linhas.Add(linha);
-            // linhas.Add(new List<string> { (i+1).ToString(), "Quantidade solicitada", "Produto", "", "Quantidade ofertada", "Valor unitário", "Valor total", "", "Quantidade ofertada", "Valor unitário", "Valor total" });
-        }
-
-        // ListaItem item = new();
-        // bool temMais = _ListaItemListEnumerator.MoveNext();
-        // if(temMais)
-        // {
-        //     item = _ListaItemListEnumerator.Current;
-        // }
-        // Console.WriteLine("item");
-        // Console.WriteLine(item.Quantidade);
-        // Console.WriteLine(item.Descricao);
-        
-        // temMais = _ListaItemListEnumerator.MoveNext();
-        // if(temMais)
-        // {
-        //     item = _ListaItemListEnumerator.Current;
-        // }
-        // Console.WriteLine("item");
-        // Console.WriteLine(item.Quantidade);
-        // Console.WriteLine(item.Descricao);
+        tabelaComparativa = CriarTabelaComparativa(_ListaItemList, _OrcamentoItemList);
+        ImprimirResultado(tabelaComparativa);
 
         await InvokeAsync(StateHasChanged);
     }
-    
+
     // ---------------- SELECT TABLE ListaItem
     protected List<ListaItem>? _ListaItemList { get; set; } = new();
     protected async Task GetListaItem(int ListaId)
     {
-        _ListaItemList = (List<ListaItem>?) await ListaItensService.SelectAllByListaId(ListaId);
+        _ListaItemList = (List<ListaItem>?)await ListaItensService.SelectAllByListaId(ListaId);
         // await InvokeAsync(StateHasChanged);
     }
 
@@ -125,7 +46,7 @@ public partial class Comparativo
     protected List<OrcamentoView>? _OrcamentoViewList { get; set; } = new();
     protected async Task GetOrcamentoView(int ListaId)
     {
-        _OrcamentoViewList = (List<OrcamentoView>?) await OrcamentoViewService.SelectAllByListaId(ListaId);
+        _OrcamentoViewList = (List<OrcamentoView>?)await OrcamentoViewService.SelectAllByListaId(ListaId);
         // await InvokeAsync(StateHasChanged);
     }
 
@@ -136,9 +57,77 @@ public partial class Comparativo
         _OrcamentoItemList = new();
         foreach (var item in OrcamentoViewList)
         {
-            List<OrcamentoItem>? aux = (List<OrcamentoItem>?) await OrcamentoItemService.SelectByOrcamentoId(item.OrcamentoId);
+            List<OrcamentoItem>? aux = (List<OrcamentoItem>?)await OrcamentoItemService.SelectByOrcamentoId(item.OrcamentoId);
             _OrcamentoItemList.Add(aux);
         }
     }
-    
+
+    static List<List<string>> CriarTabelaComparativa(List<ListaItem> listaItemList, List<List<OrcamentoItem>> orcamentoItemListList)
+    {
+        var result = new List<List<string>>();
+        var distinctBudgetIds = orcamentoItemListList.SelectMany(x => x.Select(y => y.OrcamentoId)).Distinct();
+
+        //iterating over each item of the list
+        foreach (var item in listaItemList)
+        {
+            var itemOrcamento = new List<string> {
+                item?.Quantidade?.ToString(),
+                item?.Descricao
+            };
+            foreach (var budgetId in distinctBudgetIds)
+            {
+                var budget = orcamentoItemListList.SelectMany(x => x.Where(y => y.ListaItemId == item.Id && y.OrcamentoId == budgetId)).FirstOrDefault();
+                itemOrcamento.Add(budget != null ? budget?.Quantidade?.ToString() : " ");
+                itemOrcamento.Add(budget != null ? budget?.Preco?.ToString() : " ");
+                itemOrcamento.Add(budget != null ? (budget?.Quantidade * budget?.Preco ).ToString() : " ");
+            }
+            result.Add(itemOrcamento);
+        }
+
+        //iterating over each budget
+        foreach (var budgetId in distinctBudgetIds)
+        {
+            var budgetItens = orcamentoItemListList.SelectMany(x => x.Where(y => !listaItemList.Any(z => z.Id == y.ListaItemId) && y.OrcamentoId == budgetId)).ToList();
+            foreach (var budget in budgetItens)
+            {
+                var itemOrcamento = new List<string> { " " };
+                foreach (var id in distinctBudgetIds)
+                {
+                    if (id == budget.OrcamentoId)
+                    {
+                        itemOrcamento.Add(budget?.Quantidade?.ToString());
+                        itemOrcamento.Add(budget?.Preco?.ToString());
+                        itemOrcamento.Add( (budget?.Quantidade * budget?.Preco ).ToString());
+                    }
+                    else
+                    {
+                        itemOrcamento.Add(" ");
+                        itemOrcamento.Add(" ");
+                        itemOrcamento.Add(" ");
+                    }
+                }
+                result.Add(itemOrcamento);
+            }
+        }
+        return result;
+    }
+
+    static void ImprimirResultado(List<List<string>> resultado)
+    {
+        // Find the max lenght of string in each column
+        var columnWidths = Enumerable.Range(0, resultado.First().Count)
+            .Select(i => resultado.Max(e => e[i].Length))
+            .ToArray();
+        // printing the result
+        foreach (var row in resultado)
+        {
+            for (int i = 0; i < row.Count; i++)
+            {
+                Console.Write(" | ");
+                Console.Write(row[i].PadRight(columnWidths[i] + 1));
+            }
+            Console.WriteLine();
+        }
+    }
+
 }
