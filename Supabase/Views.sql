798cfe72a37fe86081cc45e4c16324ba1c91a0d3
Rhuan Barros <rhuanbarros@gmail.com>
2023-01-18 20:21:35 -0300

    udpate das views com SoftDeleted

--- Supabase/Views.sql
+++ Supabase/Views.sql
@@ -1,14 +1,20 @@
 CREATE VIEW "ListasView" AS
-SELECT "Lista"."Id" as "ListaId", "Perfil"."Id" as "PerfilId", "Perfil"."NomeCompleto", "Lista"."Endereco", "Lista"."Status", "Lista"."CreatedAt", "Lista"."SoftDelete", "Lista"."SoftDeletedAt"
+SELECT "Lista"."Id" as "ListaId", "Perfil"."Id" as "PerfilId", "Perfil"."NomeCompleto", "Lista"."Endereco", "Lista"."Status", "Lista"."CreatedAt", "Lista"."SoftDeleted", "Lista"."SoftDeletedAt"
 FROM "Lista"
 LEFT JOIN "Perfil" ON "Lista"."PerfilId" = "Perfil"."Id"
+WHERE "Perfil"."SoftDeleted" = false
+ORDER BY "Lista"."CreatedAt" DESC
 
 CREATE VIEW "OrcamentoView" AS
-SELECT "Orcamento"."Id" as "OrcamentoId", "Orcamento"."CreatedAt", "Loja"."Id" as "LojaId", "Loja"."Nome" as "LojaNome", "Orcamento"."ListaId", "Orcamento"."SolicitacaoData", "Orcamento"."SolicitacaoHora", "Orcamento"."Recebido", "Orcamento"."RecebidoData", 
-"Orcamento"."EntregaPreco", "Orcamento"."EntregaPrazo" , "Orcamento"."DescontoNoTotal" , "Orcamento"."OrcamentoAnexo" , "Orcamento"."CodigoLoja", "OrcamentoTotal"."PrecoTotal" as "PrecoTotalSemEntrega", ("Orcamento"."EntregaPreco" + "OrcamentoTotal"."PrecoTotal" )  as "PrecoTotalComEntrega", "OrcamentoTotal"."QuantidadeItens", "Orcamento"."SoftDelete", "Orcamento"."SoftDeletedAt"
+SELECT "Orcamento"."Id" as "OrcamentoId", "Orcamento"."CreatedAt", "Loja"."Id" as "LojaId", "Loja"."Nome" as "LojaNome", "Orcamento"."ListaId", 
+"Orcamento"."SolicitacaoData", "Orcamento"."SolicitacaoHora", "Orcamento"."Recebido", "Orcamento"."RecebidoData", "Orcamento"."EntregaPreco", 
+"Orcamento"."EntregaPrazo" , "Orcamento"."DescontoNoTotal" , "Orcamento"."OrcamentoAnexo" , "Orcamento"."CodigoLoja", 
+"OrcamentoTotal"."PrecoTotal" as "PrecoTotalSemEntrega", ("Orcamento"."EntregaPreco" + "OrcamentoTotal"."PrecoTotal" )  as "PrecoTotalComEntrega", 
+"OrcamentoTotal"."QuantidadeItens", "Orcamento"."SoftDeleted", "Orcamento"."SoftDeletedAt"
 FROM "Orcamento"
 LEFT JOIN "Loja" ON "Orcamento"."LojaId" = "Loja"."Id"
 JOIN "OrcamentoTotal" ON "Orcamento"."Id" = "OrcamentoTotal"."OrcamentoId"
+WHERE "Loja"."SoftDeleted" = false
 ORDER BY "PrecoTotalComEntrega" ASC
 
 CREATE VIEW "OrcamentoTotal" AS
