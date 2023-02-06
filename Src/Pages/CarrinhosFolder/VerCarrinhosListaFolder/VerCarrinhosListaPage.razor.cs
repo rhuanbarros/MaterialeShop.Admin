7d8ca70a4a9f6cd087b5da3c362c90116efaf177
Rhuan Barros <rhuanbarros@gmail.com>
2023-02-06 15:16:18 -0300

    VerCarrinhosListaPage

---
+++ Src/Pages/CarrinhosFolder/VerCarrinhosListaFolder/VerCarrinhosListaPage.razor.cs
@@ -0,0 +1,12 @@
+using MaterialeShop.Admin.Src.Dtos;
+using MaterialeShop.Admin.Src.Services;
+using MaterialeShop.Admin.Src.Shared;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace MaterialeShop.Admin.Src.Pages.CarrinhosFolder.VerCarrinhosListaFolder;
+
+public partial class VerCarrinhosListaPage
+{
+    
+}
